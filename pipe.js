a937e0541cea03d4c135efdadb7a46103ee23e2c
BenAlaa <ahmed.alaa0794@gmail.com>
2020-12-21 04:11:16 +0200

    inhance pipe

--- pipe.js
+++ pipe.js
@@ -1,4 +1,5 @@
 const _ = require('lodash');
+const fp = require('lodash/fp')
 const xmljs = require('xml-js');
 const fs = require("fs");
 
@@ -34,8 +35,59 @@ const transformToJsObject = xmlData =>
 //Finally, let’s sort by price amount.
 const sortByPrice = booksArray => _.sortBy(booksArray, "price");
 
+
+
+
+// console.log(JSON.stringify(sortByPrice(transformToJsObject(gettingWsData))));
+
+
 (async() => {
     const xmlData = await gettingWsData();
+
+    ///////// 1 /////////
+    // we can do this like this
+    // let booksArray = transformToJsObject(xmlData);
+    // booksArray = sortByPrice(booksArray);
+    // const jsonData = JSON.stringify(booksArray);
+    // console.log(jsonData);
+
+
+
+    ////// 2 ////////////
+    // also we can do this
+    // console.log(JSON.stringify(sortByPrice(transformToJsObject(xmlData))));
+
+
+
+
+    /////// 3 ///////
+    // using flow method (lodash standard API)
+    // _.flow(
+    //     transformToJsObject,
+    //     sortByPrice,
+    //     JSON.stringify,
+    //     console.log
+    //  )(xmlData);
+
+
+
+
+
+    ////////// 4 //////////
+    // using fp.pipe method (lodash FP API)
+    // fp.pipe(
+    //     transformToJsObject,
+    //     sortByPrice,
+    //     JSON.stringify,
+    //     console.log
+    //  )(xmlData);
+
+
+
+
+
+    //////// 5 ///////
+    // using our pipe method
     pipe(
         transformToJsObject,
         sortByPrice,
